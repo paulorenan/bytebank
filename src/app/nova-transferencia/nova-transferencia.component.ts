6becc51d6ea17963a864ee26a54f44b728b2bd07
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-07-04 17:18:29 -0300

    recebendo dados

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-nova-transferencia",
@@ -6,6 +6,8 @@ import { Component } from "@angular/core";
   styleUrls: ["./nova-transferencia.component.scss"]
 })
 export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
@@ -19,5 +21,6 @@ export class NovaTransferenciaComponent {
     console.log("Transferindo...");
     console.log("Valor: " + this.valor);
     console.log("Destino: " + this.destino);
+    this.aoTransferir.emit({ valor: this.valor, destino: this.destino });
   }
 }
